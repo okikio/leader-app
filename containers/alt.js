21c23576b430d02c6d0a68cf80f291552c639470
whitelight <femikiki@gmail.com>
2018-07-07 22:33:03 -0400

    Delete alt.js

--- containers/alt.js
+++
@@ -1,8 +0,0 @@
-var _ = require("underscore");
-let pick = require("../util/pick");
-
-// Shared similarites between alt containers
-module.exports = function(alt) {
-    var value = { "alt":  pick(alt, "An iamge of a bustling city.") };
-    return _.extend({}, value);
-};
