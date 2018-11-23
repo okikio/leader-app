2b86692c15c62bbe7d9312c02133f8a7d2d653ec
whitelightG <femikiki@gmail.com>
2018-11-23 17:57:58 +0000

    Smooth loader transition

--- client/js/main.js
+++ client/js/main.js
@@ -10,14 +10,6 @@ var Image = window.Image,
     FootTop = $(".footer-top"),
     NavList = $(".navbar-list"),
     LoadImg = $("load-img");
-$(document).ready(function() {
-    setTimeout(function() {
-        $("div.cover").removeClass("load");
-        $("div.cover").on("click", function() {
-            $(this).removeClass("load");
-        });
-    }, 3000);
-});
 // from: https://github.com/jserz/js_piece/blob/master/DOM/ChildNode/before()/before().md
 // Polyfill for IE
 (function(arr) {
@@ -40,7 +32,7 @@ $(document).ready(function() {
             }
         });
     });
-})([Element.prototype, CharacterData.prototype, DocumentType.prototype]),
+})([Element.prototype, CharacterData.prototype, DocumentType.prototype]);
 Menu.on("click", function() {
     return NavList.toggleClass("navbar-list-show"), Navbar[(window.scrollY < 50 && $(window).width() < 700 && !NavList.hasClass("navbar-list-show") ? 'remove' : 'add') + 'Class']("navbar-focus")
 });
@@ -50,21 +42,27 @@ FootTop.on("click", function() {
 $(window).scroll(function() {
     Navbar[(window.scrollY > 50 || NavList.hasClass("navbar-list-show") && $(window).width() < 700 ? 'add' : 'remove') + 'Class']("navbar-focus")
 });
-LoadImg.each(function(o, a) {
-    var t = new Image,
-        n = a.getAttribute("src"),
-        e = a.getAttribute("alt"),
-        r = a.getAttribute("class");
-    t.onload = function() {
-            var o = $(t);
-            o.attr("alt", e), o.attr("class", r), a.before(o.get(0)), $(a).remove()
-        },
-        t.onerror = function(o) {
-            console.log("One of the images didn't load " + o)
-        }, t.src = n
-});
 $(document).ready(function() {
-    $("a[href^='/']").click(function() {
-        $("div.cover").addClass("load")
-    })
+    $("div.cover").on("click", function() {
+        $(this).removeClass("load");
+    });
+    setTimeout(function() {
+        $("div.cover").removeClass("load");
+        $("a[href^='/']").click(function() {
+            $("div.cover").addClass("load")
+        })
+    }, 1500);
+    LoadImg.each(function(o, a) {
+        var t = new Image,
+            n = a.getAttribute("src"),
+            e = a.getAttribute("alt"),
+            r = a.getAttribute("class");
+        t.onload = function() {
+                var o = $(t);
+                o.attr("alt", e), o.attr("class", r), a.before(o.get(0)), $(a).remove()
+            },
+            t.onerror = function(o) {
+                console.log("One of the images didn't load " + o)
+            }, t.src = n
+    });
 });
