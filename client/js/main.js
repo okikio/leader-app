81758b25212030e621612ee6fa35f5a8af2b37ff
whitelightG <femikiki@gmail.com>
2018-11-23 13:34:06 +0000

    Loading addition

--- client/js/main.js
+++ client/js/main.js
@@ -10,6 +10,14 @@ var Image = window.Image,
     FootTop = $(".footer-top"),
     NavList = $(".navbar-list"),
     LoadImg = $("load-img");
+$(document).ready(function() {
+    setTimeout(function() {
+        $("div.cover").removeClass("load");
+        $("div.cover").on("click", function() {
+            $(this).removeClass("load");
+        });
+    }, 3000);
+});
 // from: https://github.com/jserz/js_piece/blob/master/DOM/ChildNode/before()/before().md
 // Polyfill for IE
 (function(arr) {
@@ -34,32 +42,29 @@ var Image = window.Image,
     });
 })([Element.prototype, CharacterData.prototype, DocumentType.prototype]),
 Menu.on("click", function() {
-        return NavList.toggleClass("navbar-list-show"), Navbar[(window.scrollY < 50 && $(window).width() < 700 && !NavList.hasClass("navbar-list-show") ? 'remove' : 'add') + 'Class']("navbar-focus")
-    }),
-    FootTop.on("click", function() {
-        Body.animate({ scrollTop: 0 }, 500)
-    }),
-    $(window).scroll(function() {
-        Navbar[(window.scrollY > 50 || NavList.hasClass("navbar-list-show") && $(window).width() < 700 ? 'add' : 'remove') + 'Class']("navbar-focus")
-    }),
-    $("div.cover").on("click", function () { 
-        $(this).removeClass("load")
-    }),
-    LoadImg.each(function(o, a) {
-        var t = new Image,
-            n = a.getAttribute("src"),
-            e = a.getAttribute("alt"),
-            r = a.getAttribute("class");
-        t.onload = function() {
-                var o = $(t);
-                o.attr("alt", e), o.attr("class", r), a.before(o.get(0)), $(a).remove()
-            },
-            t.onerror = function(o) {
-                console.log("One of the images didn't load " + o)
-            }, t.src = n
-    }),
-    $(document).ready(function() {
-        $("a[href^='/']").click(function() {
-            $("div.cover").addClass("load")
-        })
-    });
+    return NavList.toggleClass("navbar-list-show"), Navbar[(window.scrollY < 50 && $(window).width() < 700 && !NavList.hasClass("navbar-list-show") ? 'remove' : 'add') + 'Class']("navbar-focus")
+});
+FootTop.on("click", function() {
+    Body.animate({ scrollTop: 0 }, 500)
+});
+$(window).scroll(function() {
+    Navbar[(window.scrollY > 50 || NavList.hasClass("navbar-list-show") && $(window).width() < 700 ? 'add' : 'remove') + 'Class']("navbar-focus")
+});
+LoadImg.each(function(o, a) {
+    var t = new Image,
+        n = a.getAttribute("src"),
+        e = a.getAttribute("alt"),
+        r = a.getAttribute("class");
+    t.onload = function() {
+            var o = $(t);
+            o.attr("alt", e), o.attr("class", r), a.before(o.get(0)), $(a).remove()
+        },
+        t.onerror = function(o) {
+            console.log("One of the images didn't load " + o)
+        }, t.src = n
+});
+$(document).ready(function() {
+    $("a[href^='/']").click(function() {
+        $("div.cover").addClass("load")
+    })
+});
