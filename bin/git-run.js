4754d93e1f47f88fc3d6815e7eb3055018471509
whitelightG <femikiki@gmail.com>
2018-12-03 01:12:59 +0000

    Run Heroku using forever

--- bin/git-run.js
+++ bin/git-run.js
@@ -2,10 +2,7 @@
 var shell = require("shelljs");
 var argv = Array.from(process.argv);
 
-shell.exec("git add *");
+shell.exec("git add -A");
 shell.exec("git commit -m '" + (argv[2] || "Update") + '\'');
-shell.exec("git push origin master");
-
-shell.exec("git add *");
-shell.exec("git commit -m '" + (argv[2] || "Update") + '\'');
-shell.exec("git push heroku master");
+shell.exec("git push -u origin master");
+shell.exec("git push -u heroku master");
