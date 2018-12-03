a551135b51501120c8e312dd9c26fe1739a92645
whitelightG <femikiki@gmail.com>
2018-12-03 01:01:34 +0000

    Add heroku

--- bin/git-run.js
+++ bin/git-run.js
@@ -4,5 +4,5 @@ var argv = Array.from(process.argv);
 
 shell.exec("git add *");
 shell.exec("git commit -m '" + (argv[2] || "Update") + '\'');
-shell.exec("git push -u heroku master");
-shell.exec("git push -u origin master");
+shell.exec("git push heroku master");
+shell.exec("git push origin master");
