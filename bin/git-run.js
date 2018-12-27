a636042e11baa1dd5e422f9699cb9c01bcefb6bf
whitelightG <femikiki@gmail.com>
2018-12-27 20:03:09 +0000

    Update

--- bin/git-run.js
+++ bin/git-run.js
@@ -5,4 +5,4 @@ var argv = Array.from(process.argv);
 shell.exec("git add -A");
 shell.exec("git commit -m '" + (argv[2] || "Update") + '\'');
 shell.exec("git push -u origin master");
-shell.exec("git push -u heroku master");
+// shell.exec("git push -u heroku master");
