342a7f34f21d93dfbac403976d6f490614b10264
whitelightG <femikiki@gmail.com>
2019-04-30 01:06:28 +0000

    Update

--- client/js/main.js
+++ client/js/main.js
@@ -238,53 +238,6 @@ if ('serviceWorker' in navigator) {
         }
     });
 }
-else if ('applicationCache' in window) {
-    var appCache = window.applicationCache,
-        status;
-    appCache.update(); // Attempt to update the user's cache.
-    status = (function() {
-        switch (appCache.status) {
-            case appCache.UNCACHED: // UNCACHED == 0
-                return 'UNCACHED';
-                break;
-            case appCache.IDLE: // IDLE == 1
-                return 'IDLE';
-                break;
-            case appCache.CHECKING: // CHECKING == 2
-                return 'CHECKING';
-                break;
-            case appCache.DOWNLOADING: // DOWNLOADING == 3
-                return 'DOWNLOADING';
-                break;
-            case appCache.UPDATEREADY: // UPDATEREADY == 4
-                appCache.swapCache(); // The fetch was successful, swap in the new cache.
-                return 'UPDATEREADY';
-                break;
-            case appCache.OBSOLETE: // OBSOLETE == 5
-                return 'OBSOLETE';
-                break;
-            default:
-                return 'UKNOWN CACHE STATUS';
-                break;
-        }
-    })();
-
-    // Check if a new cache is available on page load.
-    window.addEventListener("load", function() {
-        window.applicationCache.addEventListener('updateready', function(e) {
-            if (status == 'UPDATEREADY') {
-                // Browser downloaded a new app cache.
-                window.applicationCache.swapCache();
-                if (confirm('A new version of this site is available. Load it?')) {
-                    window.location.reload();
-                }
-            }
-            else { // Manifest didn't changed. Nothing new to server.
-                console.log("No Cache Change.")
-            }
-        }, false);
-    }, false);
-}
 else {
     console.log("No Service Worker's or App Cache Support")
 }
